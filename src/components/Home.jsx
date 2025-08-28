c19818ad88ca13f1b14d1690356f724867344d3e
Vinay <uttekarvinay123@gmail.com>
2025-08-28 12:25:31 +0530

    add image to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,6 +9,11 @@ const Home = () => {
         <h1>Welcome to the Home Page</h1>
         <p>This is the home page of our React application.</p>
       </div>
+
+      <img
+        src="https://www.northerntrailoutfitters.com/on/demandware.static/-/Library-Sites-NTO-SFRASharedLibrary/default/dwbc0ae950/AdobeStock_207497778.png"
+        height="500px"
+      />
       <button onClick={() => navigate("/about")}>Go to About Page</button>
     </>
   );
