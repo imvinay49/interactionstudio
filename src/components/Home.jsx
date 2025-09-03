84dd0e16644886d41dea4872545b3eae9a13d907
Vinay <uttekarvinay123@gmail.com>
2025-09-03 15:28:34 +0530

    add crousel to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -42,6 +42,49 @@ const Home = () => {
       </div>
       <button onClick={() => navigate("/about")}>Go to About Page</button>
       <div id="banner"></div>
+
+      <h1>Crousel Section</h1>
+      <br></br>
+      <div
+        id="carouselExampleControls"
+        class="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img
+              src="https://www.northerntrailoutfitters.com/on/demandware.static/-/Library-Sites-NTO-SFRASharedLibrary/default/dwd9df3c9c/images/homepage/home-promo-01-800-600.png"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src="https://www.northerntrailoutfitters.com/on/demandware.static/-/Library-Sites-NTO-SFRASharedLibrary/default/dwa2d9f633/images/homepage/home-new-styles-hero-450-600@2x.jpg"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
     </>
   );
 };
